a93268fe7dbdc68ac9734d37dc382f690524fec4
Fatima Altaf <fatima.altaf@gmail.com>
2020-02-10 12:27:32 -0500

    initial commit

---
+++ src/components/Layouts/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const Layout = (props) => {
+
+    <div>
+    Toolbar, sidedrawer, Backdrop
+  </div>
+  <main>
+    {props.children}
+  </main>
+};
