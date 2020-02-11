4a776f2958f82e60443ee6c664cb4a3d38a5a35a
Fatima Altaf <fatima.altaf@gmail.com>
2020-02-11 13:50:24 -0500

    create basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/Layouts/Layout';
 
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -3,10 +3,16 @@ import Aux from '../../hoc/Aux';
 
 const Layout = (props) => {
 
+  return (
+    <Aux>
     <div>
-    Toolbar, sidedrawer, Backdrop
-  </div>
-  <main>
-    {props.children}
-  </main>
+        Toolbar, sidedrawer, Backdrop
+      </div>
+      <main>
+        {props.children}
+      </main>
+    </Aux>
+  )
 };
+
+export default Layout;
