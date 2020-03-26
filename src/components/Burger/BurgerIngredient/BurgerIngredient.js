7fea217661f13b5539289a6ed3d2df56e4410ce9
Fatima Altaf <fatima.altaf@gmail.com>
2020-03-25 21:50:49 -0400

    burger ingredient component form

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import "./BurgerIngredient.css";
 
-const BurgerIngredient = props => {
-  let ingredient = null;
+class BurgerIngredient extends Component {
+  render() {
+    let ingredient = null;
 
-  switch (props.type) {
-    case "bread-bottom":
-      ingredient = <div className="BreadBottom"></div>;
-      break;
-    case "bread-top":
-      ingredient = (
-        <div className="BreadTop">
-          <div className="Seeds1"></div>
-          <div className="Seeds2"></div>
-        </div>
-      );
-      break;
-    case "meat":
-      ingredient = <div className="Meat"></div>;
-      break;
-    case "cheese":
-      ingredient = <div className="Cheese"></div>;
-      break;
-    case "salad":
-      ingredient = <div className="Salad"></div>;
-      break;
-    case "bacon":
-      ingredient = <div className="Beef-Bacon"></div>;
-      break;
-    default:
-      ingredient = null;
+    // Dynamic ingredient
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className="BreadBottom"></div>;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className="BreadTop">
+            <div className="Seeds1"></div>
+            <div className="Seeds2"></div>
+          </div>
+        );
+        break;
+      case "meat":
+        ingredient = <div className="Meat"></div>;
+        break;
+      case "cheese":
+        ingredient = <div className="Cheese"></div>;
+        break;
+      case "salad":
+        ingredient = <div className="Salad"></div>;
+        break;
+      case "bacon":
+        ingredient = <div className="Beef-Bacon"></div>;
+        break;
+      default:
+        ingredient = null;
+    }
+    return ingredient;
   }
-  return ingredient;
+}
+
+// Prop type validation: If use ingredient component is used w/o passing a type, will get an error
+BurgerIngredient.propTypes = {
+  type: PropTypes.string
 };
 
 export default BurgerIngredient;
