72d983825746fafcff0e7a0107c2d26c447b9a16
Fatima Altaf <fatima.altaf@gmail.com>
2020-03-26 00:07:07 -0400

    burger ingredients render to dom but not dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,6 +7,9 @@ const Burger = props => {
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
