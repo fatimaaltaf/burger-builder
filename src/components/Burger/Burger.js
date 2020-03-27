7f6eb4d7195f6ec6b26abdf25d8230173fdf9c31
Fatima Altaf <fatima.altaf@gmail.com>
2020-03-26 22:25:40 -0400

    if array is empty add message to add ingredients feature

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,15 +5,19 @@ import "./Burger.css";
 
 const Burger = props => {
   // Transform object of key value pairs into burger ingredients where value of
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])]
-      .map((_, i) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
-      })
-      .reduce((arr, el) => {
-        return arr.concat(el);
-      }, []);
-  });
+      });
+    }) //flattens array
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
 
   console.log(transformedIngredients);
 
