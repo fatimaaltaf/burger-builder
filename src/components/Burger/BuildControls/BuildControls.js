f1f0377ba290287c1731986353aa0f6912a8c8bc
Fatima Altaf <fatima.altaf@gmail.com>
2020-03-27 21:56:30 -0400

    build burger controls with css

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
+const BuildControls = props => (
+  <div className="BuildControls">
+    {controls.map(control => (
+      <BuildControl key={control.label} label={control.label} />
+    ))}
+  </div>
+);
+
+export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   };
 
@@ -18,7 +19,9 @@ class BurgerBuilder extends Component {
         <div>
           <Burger ingredients={this.state.ingredients} />
         </div>
-        <div>Build Controls</div>
+        <div>
+          <BuildControls />
+        </div>
       </Aux>
     );
   }
